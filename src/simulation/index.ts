9b38261e4d41ac0c6f8f4d433b77db851fff7ced
chameleonJP <deccho1026@icloud.com>
2026-09-21 05:19:55 +0900

    feat: validate player supply allocation and preview

--- src/simulation/index.ts
+++ src/simulation/index.ts
@@ -1,5 +1,7 @@
 export * from "./world.ts";
 export { createBattle, stepBattle, canSupplyProduce, movementMultiplier } from "./battle.ts";
+export type { CreateBattleOptions } from "./battle.ts";
+export { getPlayerSupplyPreview } from "./common-supply.ts";
 export { chooseEnemyIntent } from "./enemy-rules.ts";
 export * from "./r2b-bridge.ts";
 export type { BattleState, BattleCommand } from "../domain/battle.ts";
